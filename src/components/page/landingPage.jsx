0897d2c95b7cb171e445efe157efcb25cbfdbd4e
LeeYounSung <chance07236@naver.com>
2024-02-06 00:10:17 +0900

    feat: 폴더 생성

---
+++ src/components/page/landingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Main } from '../main';
+import { Banner } from '../banner';
+
+export const LandingPage = () => {
+  return (
+    <div className='landing-page'>
+      <Banner />
+      <Main />
+    </div>
+  )
+}
